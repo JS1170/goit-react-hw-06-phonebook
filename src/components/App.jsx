9faa92d0a7b1d78ef5cef2b45faf93b2c3f24abe
Yevgen Savchuk <107425775+JS1170@users.noreply.github.com>
2022-10-13 01:01:06 +0300

    files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,61 +1,69 @@
 import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addContacts, changeFilter, deleteContacts } from 'Redux/contactsSlice';
 import ContactForm from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 
-const baseContacts = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+// const baseContacts = [
+//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+// ];
 
 
 export default function App() {
   
-  const localContacts = localStorage.getItem('keyContacts');
-  const parsLocalContacts = JSON.parse(localContacts);
+  // const localContacts = localStorage.getItem('keyContacts');
+  // const parsLocalContacts = JSON.parse(localContacts);
 
-  const [contacts, setContacts] = useState(() =>
-    parsLocalContacts?.length > 0 ? parsLocalContacts : baseContacts
-  );
+  // const [contacts, setContacts] = useState(() =>
+  //   parsLocalContacts?.length > 0 ? parsLocalContacts : baseContacts
+  // );
 
-  const [filter, setFilter] = useState('');
+  // const [filter, setFilter] = useState('');
 
-  useEffect(() => {
-    localStorage.setItem('keyContacts', JSON.stringify(contacts));
-  }, [contacts]);
+  // useEffect(() => {
+  //   localStorage.setItem('keyContacts', JSON.stringify(contacts));
+  // }, [contacts]);
+
+  const state = useSelector(state => state)
+  const dispatch = useDispatch()
 
   const submitFormValue = newContactObject => {
     if (checkName(newContactObject.name)) {
       alert(`${newContactObject.name} is already in contacts.`);
       return;
     }
-    setContacts(prevContact => [...prevContact, newContactObject]);
+    dispatch(addContacts(newContactObject))
+    // setContacts(prevContact => [...prevContact, newContactObject]);
   };
 
   const checkName = name => {
-    return contacts.find(
+    return state.contacts.find(
       contact => contact.name.toLowerCase() === name.toLowerCase()
     );
   };
 
   const filterContacts = () => {
-    return contacts.filter(contact => {
-      return contact.name.toLowerCase().includes(filter.toLowerCase());
+    return state.contacts.filter(contact => {
+      return contact.name.toLowerCase().includes(state.filter.toLowerCase());
     });
   };
 
   const deleteCurrentItem = id => {
-    setContacts(
-      contacts.filter(contact => {
-        return contact.id !== id;
-      })
-    );
+    dispatch(deleteContacts(id))
+    // setContacts(
+    //   state.contacts.filter(contact => {
+    //     return contact.id !== id;
+    //   })
+    // );
   };
 
   const onChangeFilter = event => {
-    setFilter(event.target.value);
+    // setFilter(event.target.value);
+    dispatch(changeFilter(event.target.value))
   };
 
   return (
